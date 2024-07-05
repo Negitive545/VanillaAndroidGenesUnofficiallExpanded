cce121b9eeec4894ac852ff3dae3e3448feba213
Negitive <negitive.charge545@gmail.com>
2024-07-05 03:34:25 -0600

    Initial Commit

---
+++ 1.5/Source/VAGUE/VAGUE_InternalDefs.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Steamworks;
+using Verse;
+
+namespace VAGUE
+{
+    [DefOf]
+    internal class VAGUE_InternalDefs
+    {
+        public static GeneDef VAGUE_largeMoodDecrease;
+
+        static VAGUE_InternalDefs()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(VAGUE_InternalDefs));
+        }
+    }
+}
